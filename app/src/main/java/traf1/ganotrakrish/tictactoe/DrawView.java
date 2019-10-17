d5426ea4ad783262c5906b339ef3d9238108941b
Krish <krishganotra@gmail.com>
2019-10-17 10:05:18 -0400

    Middle of animated drawing O/X

--- app/src/main/java/traf1/ganotrakrish/tictactoe/DrawView.java
+++ app/src/main/java/traf1/ganotrakrish/tictactoe/DrawView.java
@@ -26,6 +26,12 @@ public class DrawView extends View {
     float dist;
     float[] posX;
     float[] posY;
+    int backgroundColor;
+    int OColor;
+    int XColor;
+    boolean isXTurn;
+    int drawingO;
+    int drawingX;
 
     public DrawView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -34,7 +40,10 @@ public class DrawView extends View {
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
+        isXTurn = true;
         board = new int[9];
+        drawingO = -1;
+        drawingX = -1;
 
         //Helper variables to draw lines
         lineLen = getWidth()-20;
@@ -55,17 +64,20 @@ public class DrawView extends View {
         posY[1] = (float)(getHeight()/2.0);
         posY[2] = (float)(getHeight()/2.0 + dist*2.0);
 
+        backgroundColor = Color.parseColor("#404042");
+        OColor = Color.WHITE;
+        XColor = Color.WHITE;
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setColor(Color.parseColor("#404042"));
+        paint.setColor(backgroundColor);
         canvas.drawRect(0,0,getWidth(), getHeight(), paint);
 
         paint.setColor(Color.WHITE);
-
         //Draw Horizontal lines
         canvas.drawRoundRect(leftHoriz, (float)(getHeight()/2.0 - dist - lineWidth/2.0), rightHoriz, (float)(getHeight()/2.0 - dist + lineWidth/2.0), 20, 20, paint);
         canvas.drawRoundRect(leftHoriz, (float)(getHeight()/2.0 + dist - lineWidth/2.0), rightHoriz, (float)(getHeight()/2.0 + dist + lineWidth/2.0), 20, 20, paint);
@@ -74,9 +86,19 @@ public class DrawView extends View {
         canvas.drawRoundRect((float)(getWidth()/2.0 - dist - lineWidth/2.0),topVert, (float)(getWidth()/2.0 - dist + lineWidth/2.0), bottomVert, 20, 20, paint);
         canvas.drawRoundRect((float)(getWidth()/2.0 + dist - lineWidth/2.0),topVert, (float)(getWidth()/2.0 + dist + lineWidth/2.0), bottomVert, 20, 20, paint);
 
+//        if(drawingX != -1){
+//            makeX(canvas, drawingX);
+//        }
+        if(drawingO != -1){
+            makeRing(canvas, drawingO);
+        }
+
         for(int i=0; i<board.length; i++){
             if(board[i]==1){
-                canvas.drawCircle(posX[i%3], posY[i/3], dist-30, paint);
+                drawX(canvas, i);
+            }
+            else if(board[i] == 2){
+                drawRing(canvas, i);
             }
         }
 
@@ -90,42 +112,86 @@ public class DrawView extends View {
 
         if(X<getWidth()/2.0 - dist && Y<getHeight()/2.0 - dist){
             System.out.println("Top Left");
-            board[0] = 1;
+            board[0] = isXTurn ? 1: 2;
         }
         else if(X<getWidth()/2.0 + dist && Y<getHeight()/2.0 - dist){
             System.out.println("Top Middle");
-            board[1] = 1;
+            board[1] = isXTurn ? 1: 2;
         }
         else if(X>getWidth()/2.0 + dist && Y<getHeight()/2.0 - dist){
             System.out.println("Top Right");
-            board[2] = 1;
+            board[2] = isXTurn ? 1: 2;
         }
         else if(X<getWidth()/2.0 - dist && Y<getHeight()/2.0 + dist){
             System.out.println("Middle Left");
-            board[3] = 1;
+            board[3] = isXTurn ? 1: 2;
         }
         else if(X<getWidth()/2.0 - dist && Y>getHeight()/2.0 + dist){
             System.out.println("Bottom Left");
-            board[6] = 1;
+            board[6] = isXTurn ? 1: 2;
         }
         else if(X<getWidth()/2.0 + dist && Y<getHeight()/2.0 + dist){
             System.out.println("Middle Middle");
-            board[4] = 1;
+            board[4] = isXTurn ? 1: 2;
         }
         else if(X<getWidth()/2.0 + dist && Y>getHeight()/2.0 + dist){
             System.out.println("Bottom Middle");
-            board[7] = 1;
+            board[7] = isXTurn ? 1: 2;
         }
         else if(X>getWidth()/2.0 + dist && Y<getHeight()/2.0 + dist){
             System.out.println("Middle Right");
-            board[5] = 1;
+            board[5] = isXTurn ? 1: 2;
         }
         else if(X>getWidth()/2.0 + dist && Y>getHeight()/2.0 + dist){
             System.out.println("Bottom Right");
-            board[8] = 1;
+            board[8] = isXTurn ? 1: 2;
         }
+        isXTurn = !isXTurn;
 
         invalidate();
         return false;
+    }
+
+    public void makeRing(Canvas canvas, int i){
+        int marginOut = 30;
+        int marginIn = 50;
+        RectF ovalOut = new RectF(posX[i%3]-dist+marginOut, posY[i/3]-dist+marginOut, posX[i%3]+dist-marginOut, posY[i/3]+dist-marginOut);
+        RectF ovalIn = new RectF(posX[i%3]-dist+marginIn, posY[i/3]-dist+marginIn, posX[i%3]+dist-marginIn, posY[i/3]+dist-marginIn);
+
+        for(int arcLen=0; arcLen<=360; arcLen+=1){
+            paint.setColor(OColor);
+            canvas.drawArc(ovalOut, -90, arcLen, true, paint);
+            paint.setColor(backgroundColor);
+            canvas.drawArc(ovalIn, -90, arcLen, true, paint);
+            try {
+                //set time in mili
+                Thread.sleep(200);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return ;
+    }
+
+    public void drawRing(Canvas canvas, int i){
+        paint.setColor(OColor);
+
+        canvas.drawCircle(posX[i%3], posY[i/3], dist-30, paint);
+        paint.setColor(backgroundColor);
+        canvas.drawCircle(posX[i%3], posY[i/3], dist-50, paint);
+
+        return;
+    }
+    public void drawX(Canvas canvas, int i){
+        float midX = posX[i%3];
+        float midY = posY[i/3];
+
+        paint.setColor(XColor);
+        paint.setStrokeWidth(20);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        float margin = 40;
+        canvas.drawLine(midX-dist+margin, midY-dist+margin, midX+dist-margin, midY+dist-margin, paint);
+        canvas.drawLine(midX+dist-margin, midY-dist+margin, midX-dist+margin, midY+dist-margin, paint);
     }
 }
